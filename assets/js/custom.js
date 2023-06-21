e58d093f7b04ecaba233fab0c81374d55657f95d
Alex Gil <colibri.alex@gmail.com>
2023-06-21 00:18:18 -0400

    made navbar blue on scroll

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,12 +6,12 @@ document.addEventListener('scroll', () => {
     if(window.scrollY > 100) {
     //   nav.classList.add('scrolled');
       nav.classList.add('navbar-dark');
-      nav.classList.add('bg-dark');
+      nav.classList.add('scrolled');
       nav.classList.remove('navbar-light'); 
     } else {
     //   nav.classList.remove('scrolled');
       nav.classList.remove('navbar-dark');
-      nav.classList.remove('bg-dark');
+      nav.classList.remove('scrolled');
 
     }
   })
